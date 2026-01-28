a33dc03bf2ad26c59c5a96e2996e3aa9af8dfc96
thednp <thednp@users.noreply.github.com>
2026-01-28 20:20:04 +0200

    Updated server.ts

--- examples/spa/server.ts
+++ examples/spa/server.ts
@@ -1,22 +1,26 @@
 export const startProxyServer = async (port: number = 3000) => {
-  const { loadRPCConfig } = await import("vite-uni-rpc");
   const { createServer } = await import("node:http");
-  const { createRPCMiddleware } = await import("vite-uni-rpc/express");
+  // const { loadRPCConfig } = await import("vite-uni-rpc");
+  // const { createRPCMiddleware } = await import("vite-uni-rpc/express");
+  const { loadRPCConfig } = await import("../../src/index.ts");
+  const { createRPCMiddleware } = await import("../../src/express/index.ts");
   const colors = (await import("picocolors")).default;
   const rpcConfig = await loadRPCConfig();
   const middleware = createRPCMiddleware(rpcConfig);
 
   const httpServer = createServer(async (req, res) => {
-    const next = () => new Promise((resolve) => {
-      middleware(req, res, resolve);
-    });
+    const next = () =>
+      new Promise((resolve) => {
+        middleware(req, res, resolve);
+      });
     await next();
   });
 
   httpServer.listen(port, () => {
     console.log(
-      `  ${colors.green("➜")}  ${colors.bold("RPC Backend")}: ${colors.cyan("http://localhost:" + colors.bold(port + "/"))
-      }`,
+      `  ${colors.green("➜")}  ${colors.bold("RPC Backend")}: ${colors.cyan(
+        "http://localhost:" + colors.bold(port + "/"),
+      )}`,
     );
   });
-}
+};
